08fa6a9c40a8b8fe047d8e456d1ed760d0f84328
syu-kwsk <wataru1165kwsk@gmail.com>
2021-09-04 19:06:14 +0900

    gen constant kuso reply

---
+++ api/generator/gen_reply.py
@@ -0,0 +1,7 @@
+def gen_reply(input_text):
+    print("input text:", input_text)
+    reply = "だからなんですか"
+    return reply
+
+if __name__ == '__main__':
+    print(gen_reply("Hello, World!"))
--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 
+from generator import gen_reply
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = Flask(__name__)
@@ -13,6 +14,14 @@ def hello():
 def gen_reply():
     if request.method == "GET":
         return "Please send the text you want to generate a fucking lip\n"
+    else :
+        try:
+            input_text = request.form('input')
+        except KeyError:
+            input_text = "Input is must not be blank"
+
+        reply = gen_reply(input_text)
+        return reply
 
 if __name__ == "__main__":
     # Used when running locally only. When deploying to Google App
