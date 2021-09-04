cd9aa6dc226ec2c88a7c45564b3be38be42d944d
syu-kwsk <wataru1165kwsk@gmail.com>
2021-09-05 00:09:21 +0900

    calc negaposi

--- api/generator/calc_negaposi.py
+++ api/generator/calc_negaposi.py
@@ -1,2 +1,14 @@
 def calc_negaposi(text):
-    return 0
+    # Imports the Google Cloud client library
+    from google.cloud import language_v1
+
+    # Instantiates a client
+    client = language_v1.LanguageServiceClient()
+    # The text to analyze
+    document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
+
+    # Detects the sentiment of the text
+    sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
+
+    print("Sentiment: {}, {}".format(sentiment.score, sentiment.magnitude))
+    return sentiment.score
