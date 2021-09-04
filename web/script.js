956be4e1adfdd3232a6b0ef54c8ba5e446ca1e00
ailioolio <aiai4849@gmail.com>
2021-09-04 19:27:35 +0900

    try ajax

--- web/script.js
+++ web/script.js
@@ -15,4 +15,47 @@ $(function () {
     // });
   });
 
+  $('#btnsend').on('click', function () {
+    $('#result').text('通信中...');
+    // Ajax通信を開始
+    $.ajax({
+      url: '',
+      type: 'GET',
+      // dataType: 'json',
+      // フォーム要素の内容をハッシュ形式に変換
+      // data: $('form').serializeArray(),
+      timeout: 5000,
+    })
+      .done(function (data) {
+        // 通信成功時の処理を記述
+        alert('success!');
+        // document.getElementById('js-copy-text').value = data;
+        console.log(data);
+      })
+      .fail(function () {
+        // 通信失敗時の処理を記述
+        alert('failed');
+      });
+  })
+
+  // var asyncSend = function () {
+  //   $('#result').text('通信中...');
+  //   // Ajax通信を開始
+  //   $.ajax({
+  //     url: 'https://kusoreply-generator.an.r.appspot.com',
+  //     type: 'GET',
+  //     dataType: 'json',
+  //     // フォーム要素の内容をハッシュ形式に変換
+  //     data: $('form').serializeArray(),
+  //     timeout: 5000,
+  //   })
+  //     .done(function (data) {
+  //       // 通信成功時の処理を記述
+  //     })
+  //     .fail(function () {
+  //       // 通信失敗時の処理を記述
+  //       alert('failed');
+  //     });
+  // }
+
 });
