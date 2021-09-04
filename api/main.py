3f5c9f6137cbe1258e80374c104b5a1e6d7e5df8
syu-kwsk <wataru1165kwsk@gmail.com>
2021-09-04 18:22:40 +0900

    Accept post req

--- api/main.py
+++ api/main.py
@@ -1,19 +1,21 @@
-import flask
+from flask import Flask, request
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
-app = flask.Flask(__name__)
-
+app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def hello():
     """ Return a friendly HTTP greeting. """
     return "Hello World!. Deploy from github actions\n"
 
+@app.route("/gen_reply", methods=["GET", "POST"])
+def gen_reply():
+    if request.method == "GET":
+        return "Please send the text you want to generate a fucking lip\n"
 
 if __name__ == "__main__":
     # Used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
     app.run(host="localhost", port=8080, debug=True)
-
