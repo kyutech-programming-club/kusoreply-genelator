95ec9f3abdd88d73fd0462b3bbd61e19039bca9d
syu-kwsk <wataru1165kwsk@gmail.com>
2021-09-04 19:09:32 +0900

    add headers

--- api/main.py
+++ api/main.py
@@ -1,4 +1,7 @@
 from flask import Flask, request
+HEADERS= {
+    "Access-Control-Allow-Origin": "*",
+}
 
 from generator import gen_reply
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
@@ -13,15 +16,15 @@ def hello():
 @app.route("/gen_reply", methods=["GET", "POST"])
 def gen_reply():
     if request.method == "GET":
-        return "Please send the text you want to generate a fucking lip\n"
+        return ("Please send the text you want to generate a fucking lip\n", 200, HEADERS)
     else :
         try:
             input_text = request.form('input')
         except KeyError:
-            input_text = "Input is must not be blank"
+            return ("Input is must not be blank", 400, HEADERS)
 
         reply = gen_reply(input_text)
-        return reply
+        return (reply, 200, HEADERS)
 
 if __name__ == "__main__":
     # Used when running locally only. When deploying to Google App
