61db50a36aa93ee36bb601794e1b6390e405ea0d
tetuomi <kondo.tetta851@mail.kyutech.jp>
2021-09-05 07:22:23 +0900

    Generate kuso-reply from combnation of message

--- api/generator/gen_reply.py
+++ api/generator/gen_reply.py
@@ -1,7 +1,7 @@
 import random
 
 from .calc_negaposi import calc_negaposi
-from .kuso_reply_data import KUSO_REPLY
+from .kuso_reply_data import KUSO_REPLY, COMB_KUSO_REPLY1, COMB_KUSO_REPLY2
 
 def gen_reply_from_negaposi(negaposi):
     if 0 < negaposi:
@@ -13,11 +13,15 @@ def gen_reply_from_negaposi(negaposi):
 
     return reply
 
+def gen_reply_from_combination():
+    reply = random.choice(COMB_KUSO_REPLY1) + random.choice(COMB_KUSO_REPLY2)
+    return reply
+
 def gen_reply(input_text):
     print("input text:", input_text)
     negaposi = calc_negaposi(input_text)
-    # reply = "だからなんですか"
-    reply = gen_reply_from_negaposi(negaposi)
+    reply = gen_reply_from_negaposi(negaposi) + '\n'
+    reply += gen_reply_from_combination()
     return reply
 
 if __name__ == '__main__':
