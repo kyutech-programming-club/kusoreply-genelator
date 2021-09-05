64a5f25c8c3bfbd1433b7ec122a55a2430d389dc
tetuomi <kondo.tetta851@mail.kyutech.jp>
2021-09-05 14:10:01 +0900

    Update deny_sentence

--- api/generator/deny_sentence.py
+++ api/generator/deny_sentence.py
@@ -4,17 +4,31 @@ def deny_sentence(en_sentence):
     tokens = nltk.word_tokenize(en_sentence)
     tagged = nltk.pos_tag(tokens)
     denied_en_sentence = ""
-    for (token, tag) in tagged:
-        if tag[:2] == "VB" and token in ["is", "am","are", "was", "were", "be", "do", "did", "does"]:
-            denied_en_sentence += token + " not "
-        elif tag == "VBD":
-            denied_en_sentence += "did not " + token + " "
-        elif tag in ["VB", "VBP"] and token != "Let":
-            denied_en_sentence += "do not " + token + " "
-        elif tag == "VBZ":
-            denied_en_sentence += "dose not " + token + " "
-        elif token in ["Let", "Please"]:
+    for i, (token, tag) in enumerate(tagged):
+        print(tagged[i])
+        if token == "not":
             continue
+        elif token == "please":
+            reply += "do not "
+        elif i < len(tagged)-1 and tagged[i+1][0] == "not":
+            reply += token + " "
+        elif i > 0 and tagged[i-1][0] in ["not", "to"]:
+            reply += token + " "
+        elif tag[:2] == "VB" and token in ["is", "am","are", "was", "were", "be", "do", "did", "does", "'m"]:
+            reply += token + " not "
+        elif tag == "VBD":
+            reply += "did not " + token + " "
+        elif tag in ["VB", "VBP"] and token != "Let":
+            if i > 0 and tagged[i-1][1] == 'MD':
+                reply += "not " + token + " "
+            else:
+                reply += "do not " + token + " "
+        elif tag == "VBZ":
+            if i > 0 and tagged[i-1][1] == 'MD':
+                reply += "not " + token + " "
+            else:
+                reply += "does not " + token + " "
         else:
-            denied_en_sentence += token + " "
+            reply += token + " "
+
     return denied_en_sentence[:-1]
